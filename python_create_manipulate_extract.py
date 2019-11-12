c02eed0dade9257b9957308b9c1262daa4dc755a
molliewebb <molliewebb@users.noreply.github.com>
2019-11-12 11:15:31 -0600

    added parentheses for running in Py 3.x

--- python_create_manipulate_extract.py
+++ python_create_manipulate_extract.py
@@ -52,9 +52,9 @@ gdb_path = os.path.join(path,gdb_name)
 # ArcPy Help - https://pro.arcgis.com/en/pro-app/tool-reference/data-management/create-file-gdb.htm
 if arcpy.Exists(gdb_path) == False:
     arcpy.CreateFileGDB_management(path, gdb_name)
-    print gdb_name + " has been created"
+    print(gdb_name + " has been created")
 else:
-    print gdb_name + " already exists"
+    print(gdb_name + " already exists")
 
 # Sets arcpy workspace to the gdb
 arcpy.env.workspace = gdb_path
@@ -113,7 +113,7 @@ near_value_ft = "2640 FEET"
 ####     # Creates a feature class from this temp layer
 ####     # ArcPy Help = http://pro.arcgis.com/en/pro-app/tool-reference/data-management/copy-features.htm
 ####     arcpy.CopyFeatures_management (temp, fc_name)
-####     print fc_name + " feature class created"
+####     print(fc_name + " feature class created")
 ####     del temp
 
 ## CODE BLOCK 1b
@@ -125,7 +125,7 @@ near_value_ft = "2640 FEET"
 ##### Merges all of the individual crime feature classes to a single crime feature class
 ##### ArcPy Help - http://pro.arcgis.com/en/pro-app/tool-reference/data-management/merge.htm
 ####crime_fc = arcpy.Merge_management (crime_fc_list, crime_fc_name)
-####print crime_fc_name + " feature class created using Merge tool"
+####print(crime_fc_name + " feature class created using Merge tool")
 
 ## CODE BLOCK 1c
 
@@ -133,10 +133,10 @@ near_value_ft = "2640 FEET"
 ####crime_fc = os.path.join(gdb_path,crime_fc_name)
 ##### Cleans up crimes feature class by removing rows with zero coordinates
 ####loc_zero_query = crime_x_field + " > 0 AND " + crime_y_field + " > 0"
-####print loc_zero_query
+####print(loc_zero_query)
 ##### ArcPy Help - http://desktop.arcgis.com/en/arcmap/latest/tools/conversion-toolbox/feature-class-to-feature-class.htm
 ####crime_scrub_fc = arcpy.FeatureClassToFeatureClass_conversion (crime_fc, gdb_path, crime_scrub_fc_name,loc_zero_query)
-####print crime_scrub_fc_name + " feature class created"
+####print(crime_scrub_fc_name + " feature class created")
 
 ## END CODE BLOCK 1
 
@@ -149,17 +149,17 @@ near_value_ft = "2640 FEET"
 ##### Performs a tabular join between shapes and data table
 ##### ArcPy Help - http://pro.arcgis.com/en/pro-app/tool-reference/data-management/add-join.htm
 ####bg_joined = arcpy.AddJoin_management (bg_lyr, bg_shp_join_field, bg_tbl, bg_tbl_join_field,"KEEP_COMMON")
-####print "Block group shapes and block group tabular data have been joined"
+####print("Block group shapes and block group tabular data have been joined")
 ####
 ##### Exports the joined shapes and table as a feature class
 ##### ArcPy Help - http://desktop.arcgis.com/en/arcmap/latest/tools/conversion-toolbox/feature-class-to-feature-class.htm
 ####bg_joined_fc = arcpy.FeatureClassToFeatureClass_conversion (bg_joined, gdb_path, bg_fc_name)
-####print bg_fc_name + " feature class has been created"
+####print(bg_fc_name + " feature class has been created")
 ####
 ##### Reprojects the joined block groups into the same projection as the crime points
 ##### ArcPy Help - https://pro.arcgis.com/en/pro-app/tool-reference/data-management/project.htm
 ####bg_projected_fc = arcpy.Project_management(bg_joined_fc, bg_fc_state_plane_name, crime_spatial_ref)
-####print bg_fc_state_plane_name + " projected feature class has been created"
+####print(bg_fc_state_plane_name + " projected feature class has been created")
 
 ## END CODE BLOCK 2
 
@@ -193,13 +193,12 @@ near_value_ft = "2640 FEET"
 ####    bg_uc = arcpy.da.UpdateCursor(bg_projected_fc,[bg_fc_id_field,crime_type])
 ####    # The bg_shp_join_field will be the first element in the cursor result [0]
 ####    # The specific crime count attribute will be the second element in the cursor result [1]
-####    print "Working on " + crime_type + " counts"
+####    print("Working on " + crime_type + " counts")
 ####
 ####    # Iterates through the features specified in the UpdateCursor
 ####    for bg in bg_uc:
 ####     # Creates query to isolate the target feature using the ID
 ####     bg_query = bg_fc_id_field + " = '" + str(bg[0]) + "'"
-####     print bg_query
 ####     # Creates a feature layer with a single record for the target bg
 ####     # ArcPy Help - http://pro.arcgis.com/en/pro-app/tool-reference/data-management/make-feature-layer.htm
 ####     bg_temp = arcpy.MakeFeatureLayer_management (bg_projected_fc, "bg_temp", bg_query)
@@ -209,7 +208,6 @@ near_value_ft = "2640 FEET"
 ####     # Sets a variable to the count of features selected in the crimes fc
 ####     # ArcPy Help - https://pro.arcgis.com/en/pro-app/tool-reference/data-management/get-count.htm
 ####     count = arcpy.GetCount_management(crimes_selected)[0]
-####     print count
 ####     # Sets the crime count attribute to the count
 ####     bg[1] = count
 ####     # Performs the update on the feature class
@@ -234,6 +232,6 @@ near_value_ft = "2640 FEET"
 ##### Exports attribute table to csv
 ##### ArcPy Help - https://pro.arcgis.com/en/pro-app/tool-reference/conversion/table-to-table.htm
 ####arcpy.TableToTable_conversion (bg_projected_fc, path, output_csv)
-####print "The table " + output_csv + " has been created"
+####print("The table " + output_csv + " has been created")
 
 #### END CODE BLOCK 4
